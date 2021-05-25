48f18c6b964b1da95179a58e0333b0ab6cfa5183
Jack O'Hara <jack@jackohara.io>
2021-05-25 20:09:03 +0100

    Foundation for the basic styling

---
+++ components/journal-editor.tsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import ReactMde from "react-mde";
+import * as Showdown from "showdown";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import "react-mde/lib/styles/css/react-mde-all.css";
+
+const EditorContainer = styled.div`
+  flex-grow: 1;
+  margin: 0 auto;
+  width: 100%;
+  padding: 1em 2em;
+`;
+
+export interface JournalEditorProps {
+  value: string;
+  setValue: Dispatch<SetStateAction<string>>;
+}
+
+const setTextAreaHeight = () => {
+  const editorHeight =
+    document.getElementById("editor-container")?.offsetHeight ?? 0;
+
+  document.documentElement.style.setProperty(
+    "--textarea-size",
+    `${editorHeight - 100}px`
+  );
+};
+
+const converter = new Showdown.Converter({
+  tables: true,
+  simplifiedAutoLink: true,
+  strikethrough: true,
+  tasklists: true,
+});
+
+const JournalEditor = ({ value, setValue }: JournalEditorProps) => {
+  const [selectedTab, setSelectedTab] = useState<"write" | "preview">("write");
+
+  useEffect(() => {
+    setTextAreaHeight();
+  }, []);
+
+  return (
+    <EditorContainer id="editor-container">
+      <ReactMde
+        value={value}
+        onChange={setValue}
+        selectedTab={selectedTab}
+        onTabChange={setSelectedTab}
+        generateMarkdownPreview={(markdown: string) =>
+          Promise.resolve(converter.makeHtml(markdown))
+        }
+        classes={{
+          textArea: "custom-text-area",
+          reactMde: "text-area-container",
+          toolbar: "text-area-toolbar",
+          preview: "editor-preview",
+        }}
+      />
+    </EditorContainer>
+  );
+};
+
+export default JournalEditor;
---
+++ components/sidebar.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  min-width: 400px;
+  min-height: 100%;
+  background-color: var(--bg-colour-primary);
+`;
+
+const Sidebar = () => {
+  return <Container> </Container>;
+};
+
+export default Sidebar;
