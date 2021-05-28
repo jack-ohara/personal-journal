572210f478f489f9fcee5560013523aa9156de6b
Jack O'Hara <jack@jackohara.io>
2021-05-28 19:40:14 +0100

    Restructered the login - moved it to it's own page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,27 @@
 import Layout from "../components/layout";
 import AuthorisedHomePage from "../components/authorised-home-page";
-import Login from "../components/login";
 import { useEffect, useState } from "react";
 import { getGoTrue } from "../utils/go-true";
+import { useRouter } from "next/dist/client/router";
 
 const HomePage = () => {
   const auth = getGoTrue();
+  const router = useRouter();
 
   const [isAuthorised, setIsAuthorised] = useState(false);
 
   useEffect(() => {
+    router.prefetch("/login");
     const currentUser = auth.currentUser();
 
     if (!currentUser) {
-      return;
+      router.push("/login");
     }
 
     setIsAuthorised(true);
   }, []);
 
-  return (
-    <Layout>
-      {isAuthorised ? (
-        <AuthorisedHomePage auth={auth} />
-      ) : (
-        <Login auth={auth} />
-      )}
-    </Layout>
-  );
+  return <Layout>{isAuthorised && <AuthorisedHomePage auth={auth} />}</Layout>;
 };
 
 export default HomePage;
---
+++ pages/login.tsx
@@ -0,0 +1,114 @@
+import styled from "styled-components";
+import Button from "../components/button";
+import Layout from "../components/layout";
+import LoadingSpinner from "../components/loading-spinner";
+import { FormEvent, useEffect, useState } from "react";
+import { useRouter } from "next/dist/client/router";
+import { getGoTrue } from "../utils/go-true";
+
+const LoginForm = styled.form`
+  margin: auto auto;
+  padding: 1em;
+  background-color: white;
+  border-radius: 8px;
+  width: 90%;
+  max-width: 350px;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+`;
+
+const Heading = styled.h2`
+  font-size: 2em;
+  margin: 0;
+`;
+
+const HorizontalRule = styled.hr`
+  margin: 0;
+`;
+
+const Input = styled.input`
+  font-size: 1.2em;
+  padding: 0.5em;
+  border-radius: 4px;
+  border: 1px solid ${(props: InputStyleProps) => (props.error ? "red" : "")};
+`;
+
+const ErrorParagraph = styled.p`
+  color: red;
+`;
+
+interface InputStyleProps {
+  error: boolean;
+}
+
+export default function Login() {
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(false);
+  const [buttonContents, setButtonContents] =
+    useState<string | JSX.Element>("Login");
+
+  const router = useRouter();
+  const auth = getGoTrue();
+
+  useEffect(() => {
+    router.prefetch("/");
+  });
+
+  const login = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setButtonContents(<LoadingSpinner size="0.67em" />);
+    setLoginError(false);
+
+    try {
+      const user = await auth.login(emailAddress, password, true);
+
+      if (user) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.error(error);
+      setLoginError(true);
+      setPassword("");
+    } finally {
+      setButtonContents("Login");
+    }
+  };
+
+  return (
+    <Layout>
+      <LoginForm onSubmit={(e) => login(e)}>
+        <Heading>Login</Heading>
+        <HorizontalRule />
+        <Input
+          type="email"
+          placeholder="Email address"
+          value={emailAddress}
+          onChange={(e) => setEmailAddress(e.target.value)}
+          required
+          error={loginError}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          error={loginError}
+        />
+
+        {loginError && (
+          <ErrorParagraph>
+            There was a problem loging in. Check your details and try again
+          </ErrorParagraph>
+        )}
+
+        <Button type="submit" fontSize="1.3em">
+          {buttonContents}
+        </Button>
+      </LoginForm>
+    </Layout>
+  );
+}
