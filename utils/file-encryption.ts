e57efceb2fc28dfa1c04ed6b25f5e78e5d3e3e8b
Jack O'Hara <jack@jackohara.io>
2021-06-04 08:38:57 +0100

    Remove the encryption logging

--- utils/file-encryption.ts
+++ utils/file-encryption.ts
@@ -12,17 +12,9 @@ export function encryptFile(fileContents: string, password: string) {
 }
 
 export function decryptFile(data: string, password: string): string {
-  console.log(password);
-  const passwordHash = createPasswordHash(password);
-  console.log(`hash: ${passwordHash}`);
-  console.log(`data: ${data}`);
-  const bytes = CryptoJs.AES.decrypt(data, passwordHash);
-
-  console.log(`bytes: ${bytes}`);
+  const bytes = CryptoJs.AES.decrypt(data, createPasswordHash(password));
 
   const string = bytes.toString(CryptoJs.enc.Utf8);
-
-  console.log(`string: ${string}`);
 
   return JSON.parse(string).fileContents;
 }
